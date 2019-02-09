99d365a0b01cc5d7620d63c501ad1bbe3479e89a
Tom Butts <tombutts@Toms-MacBook-Pro.local>
2019-02-09 11:14:51 +0000

    More testing of recursive calculator.

--- tests/unit/models/RecursiveCombinationSolverTest.php
+++ tests/unit/models/RecursiveCombinationSolverTest.php
@@ -5,7 +5,7 @@ use app\models\RecursiveCombinationSolver;
 
 class RecursiveCombinationSolverTest extends \Codeception\Test\Unit
 {
-    protected $questionNumbers = [250, 500, 1000, 2000, 5000];
+    protected $packs = [250, 500, 1000, 2000, 5000];
 
     /**
      * @var \UnitTester
@@ -25,9 +25,30 @@ class RecursiveCombinationSolverTest extends \Codeception\Test\Unit
     public function sumProvider()
     {
         return [
-            'using pack sizes from question' => [570, $this->questionNumbers, 750],
-            'using question sizes plus a 300 pack' => [590, array_merge($this->questionNumbers, [300]), 600],
-            'using question sizes on a larger total' => [5300, $this->questionNumbers, 5500]
+            'using pack sizes from question' => [570, $this->packs, 750],
+            'using pack sizes from question (finds exactly equal sum)' => [5000, $this->packs, 5000],
+            'using question sizes plus a 300 pack' => [590, array_merge($this->packs, [300]), 600],
+            'using question sizes on a larger total' => [5300, $this->packs, 5500]
+        ];
+    }
+
+    /**
+     * @dataProvider comboProvider
+     */
+    public function testItFindsSmallestCombination($target, $numbers, $expectedCombo)
+    {
+        $solver = new RecursiveCombinationSolver();
+
+        expect($solver->calculateCombinations($target, $numbers))
+            ->equals($expectedCombo);
+    }
+
+    public function comboProvider()
+    {
+        return [
+            'using pack sizes from question' => [590, $this->packs, [500, 250]],
+            'using question sizes plus a 300 pack' => [870, array_merge($this->packs, [300]), [300, 300, 300]],
+            'using question sizes on a larger total' => [5610, $this->packs, [5000, 500, 250]]
         ];
     }
 }
